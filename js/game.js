3e2a96786de036346db5bc975fea05f9735cf153
ry2248 <92850727+ry2248@users.noreply.github.com>
2021-11-17 18:38:26 +0900

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,3 +1,10 @@
+const APPLICATION_KEY = "6cb1375d895d639274094e83f0d938f60cd7cefd7384fa1519499c92ac4b1bf3";
+const CLIENT_KEY = "708019a2c6032cfa269b2a99470fa679176298e5d8244e6dc1397251be41c566";
+const ncmb = new NCMB(APPLICATION_KEY,CLIENT_KEY);
+const DBName = "GameClass";
+
+  let GameClass = ncmb.DataStore(DBName);
+
 let timer = null;
 function init() {
   if (timer == null) {
@@ -23,9 +30,11 @@ function gameStart() {
         Max++;
         if(Max==4){
           alert("クリアです.あなたはクリアするまで、" + timer + "秒かかりました");
+          save();
+          load();
           stop();
         }
-        
+
         while(cells.firstChild ){
           cells.removeChild(cells. firstChild);
         }
@@ -55,4 +64,37 @@ function time() {
 
 function stop(){
   clearTimeout(timer);
+}
+
+function save(){
+  let test = new GameClass();
+  let key = "score";
+  let value = timer;
+
+
+  test.set(key, parseInt(value));
+  test.save()
+  .then(function(){
+  console.log("成功");
+})
+  .catch(function(err){
+  console.log("エラー発生:" + err);
+  });
+}
+
+function load(){
+  GameClass
+  .order("score")
+  .fetchAll()
+  .then(function(results){
+    if(timer<results[0].score){
+      alert("ハイスコア");
+    }else{
+      alert("クリア");
+    }
+  })
+  .catch(function(err){
+    console.log("エラー発生："+ err);
+  })
+
 }
